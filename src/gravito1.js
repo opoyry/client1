e3be40e54e24b9228ffb350ba99a3708619d7421
olli <olli.poyry@essaim.fi>
2018-03-03 20:13:18 +0200

    Add oma2.html

---
+++ src/gravito1.js
@@ -0,0 +1,26 @@
+
+    <script src="src/cookieconsent.js"></script>
+    <script>
+      window.addEventListener("load", function () {
+        window.cookieconsent.initialise({
+          "palette": {
+            "popup": {
+              "background": "#252e39"
+            },
+            "button": {
+              "background": "#14a7d0"
+            }
+          },
+          "position": "bottom-right",
+          "type": "opt-in",
+          "content": {
+            "href": "https://www.gravito.net?l=" + window.location.hostname,
+            "link": "Or click here to open privacy preferences page",
+            "message": "Enter your email address <input type=\"text\" size=\"40\" name=\"email\" value=\"\"><br/>and press the button below",
+            "allow": "Get email<br/>with link to<br/>preferences", // link to<br/>preferences to<br/>your email",
+            "dismiss": 'Continue<br/>anonymously'
+          },
+          "revokeBtn" : "<div class=\"cc-revoke {{classes}}\">Preference settings</div>"
+        })
+      });
+    </script>
